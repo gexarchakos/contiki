3ce8a1b26602c4fa17224ac39970d06f91da43e4
George Exarchakos <g.exarchakos@tue.nl>
2016-03-09 16:46:04 +0100

    examples/plexi-rpl-tsch: added support of Z1 and JN516x nodes

--- examples/plexi-rpl-tsch/common-conf.h
+++ examples/plexi-rpl-tsch/common-conf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, SICS Swedish ICT.
+ * Copyright (c) 2014, Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,125 +29,217 @@
  */
 /**
  * \author Simon Duquennoy <simonduq@sics.se>
- * \author George Exarchakos <g.exarchakos@tue.nl>
  */
 
 #ifndef __COMMON_CONF_H__
 #define __COMMON_CONF_H__
 
-#define MAC_CONFIG_NULLRDC                    0
-#define MAC_CONFIG_CONTIKIMAC                 1
-#define MAC_CONFIG_TSCH                       2
-/* Select a MAC configuration */
-#define MAC_CONFIG MAC_CONFIG_TSCH
+/* Global config flags */
 
-#undef NETSTACK_CONF_MAC
-#undef NETSTACK_CONF_RDC
-#undef NETSTACK_CONF_FRAMER
-
-#if MAC_CONFIG == MAC_CONFIG_NULLRDC
-
-#define NETSTACK_CONF_MAC     csma_driver
-#define NETSTACK_CONF_RDC     nullrdc_driver
-#define NETSTACK_CONF_FRAMER  framer_802154
-
-#elif MAC_CONFIG == MAC_CONFIG_CONTIKIMAC
-
-#define NETSTACK_CONF_MAC     csma_driver
-#define NETSTACK_CONF_RDC     contikimac_driver
-#define NETSTACK_CONF_FRAMER  contikimac_framer
-#undef   MICROMAC_CONF_AUTOACK
-#define  MICROMAC_CONF_AUTOACK 1
-
-#elif MAC_CONFIG == MAC_CONFIG_TSCH
-
-/* Set to run plexi */
-#ifndef WITH_PLEXI
-#define WITH_PLEXI 1
-#endif /* WITH_PLEXI */
-
-/* Set to enable TSCH security */
-#ifndef WITH_SECURITY
-#define WITH_SECURITY 0
-#endif /* WITH_SECURITY */
-
-#define NETSTACK_CONF_MAC     tschmac_driver
-#define NETSTACK_CONF_RDC     nordc_driver
-#define NETSTACK_CONF_FRAMER  framer_802154
-
-/* IEEE802.15.4 frame version */
-#undef FRAME802154_CONF_VERSION
-#define FRAME802154_CONF_VERSION FRAME802154_IEEE802154E_2012
-
-/* TSCH and RPL callbacks */
-#define RPL_CALLBACK_PARENT_SWITCH tsch_rpl_callback_parent_switch
-#define RPL_CALLBACK_NEW_DIO_INTERVAL tsch_rpl_callback_new_dio_interval
-#define TSCH_CALLBACK_JOINING_NETWORK tsch_rpl_callback_joining_network
-#define TSCH_CALLBACK_LEAVING_NETWORK tsch_rpl_callback_leaving_network
-
-/* TSCH logging. 0: disabled. 1: basic log. 2: with delayed
- * log messages from interrupt */
-#undef TSCH_LOG_CONF_LEVEL
+#define WITH_TSCH 1
+#define WITH_TSCH_SECURITY 0
 #define TSCH_LOG_CONF_LEVEL 2
+#define WITH_COAP_RESOURCES 1
 
-/* Do not start TSCH at init, wait for NETSTACK_MAC.on() */
+#define TSCH_CONFIG_DEFAULT                  0
+#define TSCH_CONFIG_6TISCH_MINIMAL           1
+
+#define TSCH_CONFIG TSCH_CONFIG_6TISCH_MINIMAL
+
+#undef ENABLE_COOJA_DEBUG
+#define ENABLE_COOJA_DEBUG 0
+
+#undef IEEE802154_CONF_PANID
+#define IEEE802154_CONF_PANID 0x81b2
+
+#if WITH_TSCH
+
+#undef TSCH_CONF_JOIN_MY_PANID_ONLY
+#define TSCH_CONF_JOIN_MY_PANID_ONLY 1
+
 #undef TSCH_CONF_AUTOSTART
 #define TSCH_CONF_AUTOSTART 0
 
-/* 6TiSCH minimal schedule length.
- * Larger values result in less frequent active slots: reduces capacity and saves energy. */
+#define RPL_CALLBACK_PARENT_SWITCH tsch_rpl_callback_parent_switch
+#define RPL_CALLBACK_NEW_DIO_INTERVAL tsch_rpl_callback_new_dio_interval
+
+/* RPL Trickle timer tuning */
+#undef RPL_CONF_DIO_INTERVAL_MIN
+#define RPL_CONF_DIO_INTERVAL_MIN 12 /* 4.096 s */
+
+#undef RPL_CONF_DIO_INTERVAL_DOUBLINGS
+#define RPL_CONF_DIO_INTERVAL_DOUBLINGS 2 /* Max factor: x4. 4.096 s * 4 = 16.384 s */
+
+#define TSCH_CONF_EB_PERIOD (10 * CLOCK_SECOND)
+#define TSCH_CONF_KEEPALIVE_TIMEOUT (12 * CLOCK_SECOND)
+#define RPL_CONF_OF rpl_mrhof
+
+#if TSCH_CONFIG == TSCH_CONFIG_6TISCH_MINIMAL
+/* For now, defaults used at the 2015 Prague plugtest
+ * rather than from WG drafts */
+
+#undef TSCH_SCHEDULE_CONF_WITH_6TISCH_MINIMAL
+#define TSCH_SCHEDULE_CONF_WITH_6TISCH_MINIMAL 1
+
+/* Enable all EB IEs */
+#undef TSCH_PACKET_CONF_EB_WITH_TIMESLOT_TIMING
+#define TSCH_PACKET_CONF_EB_WITH_TIMESLOT_TIMING 1
+
+#undef TSCH_PACKET_CONF_EB_WITH_HOPPING_SEQUENCE
+#define TSCH_PACKET_CONF_EB_WITH_HOPPING_SEQUENCE 1
+
+#undef TSCH_PACKET_CONF_EB_WITH_SLOTFRAME_AND_LINK
+#define TSCH_PACKET_CONF_EB_WITH_SLOTFRAME_AND_LINK 1
+
+#define TSCH_HOPPING_SEQUENCE_SINGLE_CHANNEL (uint8_t[]){20}
+
+#undef TSCH_CONF_DEFAULT_HOPPING_SEQUENCE
+/* #define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE TSCH_HOPPING_SEQUENCE_SINGLE_CHANNEL */
+#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE TSCH_HOPPING_SEQUENCE_16_16
+
 #undef TSCH_SCHEDULE_CONF_DEFAULT_LENGTH
-#define TSCH_SCHEDULE_CONF_DEFAULT_LENGTH 3
+#define TSCH_SCHEDULE_CONF_DEFAULT_LENGTH 11
 
-#if WITH_SECURITY
+#undef TSCH_CONF_MAC_MAX_FRAME_RETRIES
+#define TSCH_CONF_MAC_MAX_FRAME_RETRIES 3
 
-/* Enable security */
-#undef LLSEC802154_CONF_SECURITY_LEVEL
-#define LLSEC802154_CONF_SECURITY_LEVEL 1
-/* TSCH uses explicit keys to identify k1 and k2 */
+#undef TSCH_CONF_EB_PERIOD
+#define TSCH_CONF_EB_PERIOD (2 * CLOCK_SECOND)
+
+#undef TSCH_CONF_KEEPALIVE_TIMEOUT
+#define TSCH_CONF_KEEPALIVE_TIMEOUT (5 * CLOCK_SECOND)
+
+#undef TSCH_CONF_DESYNC_THRESHOLD
+#define TSCH_CONF_DESYNC_THRESHOLD (24 * CLOCK_SECOND)
+
+#undef RPL_MIN_HOPRANKINC
+#define RPL_MIN_HOPRANKINC          256
+
+#undef FRAME802154_CONF_SUPPR_SEQNO
+#define FRAME802154_CONF_SUPPR_SEQNO 0
+
+#undef TSCH_PACKET_CONF_EACK_WITH_SRC_ADDR
+#define TSCH_PACKET_CONF_EACK_WITH_SRC_ADDR 1
+
+#undef TSCH_PACKET_CONF_EACK_WITH_DEST_ADDR
+#define TSCH_PACKET_CONF_EACK_WITH_DEST_ADDR 1
+
+#endif /* TSCH_CONFIG == TSCH_CONFIG_6TISCH_MINIMAL */
+
+#endif
+
+/* CoAP */
+
+#undef COAP_SERVER_PORT
+#define COAP_SERVER_PORT 5684
+
+#undef COAP_OBSERVE_RETURNS_REPRESENTATION
+#define COAP_OBSERVE_RETURNS_REPRESENTATION 1
+
+/* RPL */
+
+#undef UIP_CONF_ROUTER
+#define UIP_CONF_ROUTER                 1
+
+/* RPL storing mode */
+#undef RPL_CONF_MOP
+#define RPL_CONF_MOP RPL_MOP_STORING_NO_MULTICAST
+
+/* Default link metric */
+#undef RPL_CONF_INIT_LINK_METRIC
+#define RPL_CONF_INIT_LINK_METRIC 2 /* default 5 */
+
+#define RPL_CONF_MAX_INSTANCES    1 /* default 1 */
+#define RPL_CONF_MAX_DAG_PER_INSTANCE 1 /* default 2 */
+
+/* No RA, No NA */
+#undef UIP_CONF_ND6_SEND_NA
+#define UIP_CONF_ND6_SEND_NA 0
+
+#undef UIP_CONF_ND6_SEND_RA
+#define UIP_CONF_ND6_SEND_RA 0
+
+#undef UIP_CONF_TCP
+#define UIP_CONF_TCP             0
+#undef UIP_CONF_DS6_ADDR_NBU
+#define UIP_CONF_DS6_ADDR_NBU    1
+#undef UIP_CONF_FWCACHE_SIZE
+#define UIP_CONF_FWCACHE_SIZE    1
+#undef UIP_CONF_UDP_CHECKSUMS
+#define UIP_CONF_UDP_CHECKSUMS   1
+
+/* Link-layer security */
+
+/* Even when link-layer security is needed, we do not use a LLSEC layer, as it does not
+ * allow to secure MAC-layer packets, nor can run encrypt/decrupt from interrupt.
+ * Instead, we call AES-CCM* primitives directly from TSCH */
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC nullsec_driver
+
+#if WITH_TSCH_SECURITY
+/* Set security level to the maximum, even if unused, to all crypto code */
+#define LLSEC802154_CONF_SECURITY_LEVEL 7
+/* Attempt to associate from both secured and non-secured EBs */
+#define TSCH_CONF_JOIN_SECURED_ONLY 0
+/* We need explicit keys to identify k1 and k2 */
 #undef LLSEC802154_CONF_USES_EXPLICIT_KEYS
 #define LLSEC802154_CONF_USES_EXPLICIT_KEYS 1
-/* TSCH uses the ASN rather than frame counter to construct the Nonce */
+/* TSCH uses the ASN to construct the Nonce */
 #undef LLSEC802154_CONF_USES_FRAME_COUNTER
 #define LLSEC802154_CONF_USES_FRAME_COUNTER 0
+#endif /* WITH_TSCH_SECURITY */
 
-#endif /* WITH_SECURITY */
+#if WITH_TSCH
 
-#if WITH_PLEXI
+#undef FRAME802154_CONF_VERSION
+#define FRAME802154_CONF_VERSION FRAME802154_IEEE802154E_2012
 
-/* See apps/plexi/README.md for more plexi configuration options */
-#define TSCH_SCHEDULE_CONF_WITH_6TISCH_MINIMAL 1 /* No 6TiSCH minimal schedule */
-#define TSCH_CONF_WITH_LINK_SELECTOR 1 /* Orchestra requires per-packet link selection */
+/* Contiki netstack: MAC */
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC     tschmac_driver
 
-#endif /* WITH_PLEXI */
+/* Contiki netstack: RDC */
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC     nordc_driver
 
+#else /* No TSCH, use Csma+NullRDC with ACK */
 
-#else
+/* Contiki netstack: MAC */
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC     csma_driver
 
-#error Unsupported MAC configuration
+/* Contiki netstack: RDC */
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC     nullrdc_driver
 
-#endif /* MAC_CONFIG */
+#undef RF_CHANNEL
+#define RF_CHANNEL 26
 
-/* IEEE802.15.4 PANID and channel */
+#undef CC2420_CONF_AUTOACK
+#define CC2420_CONF_AUTOACK 1
 
-#undef IEEE802154_CONF_PANID
-#define IEEE802154_CONF_PANID 0xabcd
+#undef MICROMAC_CONF_AUTOACK
+#define MICROMAC_CONF_AUTOACK 1
 
-#undef MICROMAC_CONF_CHANNEL
-#define MICROMAC_CONF_CHANNEL 26
+/* increase internal radio buffering */
+#undef MIRCOMAC_CONF_BUF_NUM
+#define MIRCOMAC_CONF_BUF_NUM 4
 
-/* UART Configuration */
+#endif
 
-#undef UART_HW_FLOW_CTRL
-#define UART_HW_FLOW_CTRL 0
+#undef CONTIKI_VERSION_STRING
+#define CONTIKI_VERSION_STRING "Contiki 3.0-PLEXI"
 
-#undef UART_XONXOFF_FLOW_CTRL
-#define UART_XONXOFF_FLOW_CTRL 1
+#if CONTIKI_TARGET_SKY || CONTIKI_TARGET_Z1
 
-#undef UART_BAUD_RATE
-#define UART_BAUD_RATE UART_RATE_1000000
+#include "common-conf-sky-z1.h"
 
-#undef REST_MAX_CHUNK_SIZE
-#define REST_MAX_CHUNK_SIZE      256
+#endif /* CONTIKI_TARGET_SKY || CONTIKI_TARGET_Z1 */
+
+#if CONTIKI_TARGET_JN516X
+
+#include "common-conf-jn516x.h"
+
+#endif /* CONTIKI_TARGET_JN516X */
 
 #endif /* __COMMON_CONF_H__ */
