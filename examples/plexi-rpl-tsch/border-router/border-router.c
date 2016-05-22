49deebc8122842bd2af548bfec7d863282b9044d
George Exarchakos <g.exarchakos@tue.nl>
2016-05-22 22:56:48 +0200

    enabled link statistics and prefix from tunslip for plexi-rpl-tsch example

--- examples/plexi-rpl-tsch/border-router/border-router.c
+++ examples/plexi-rpl-tsch/border-router/border-router.c
@@ -81,7 +81,7 @@ PROCESS_THREAD(border_router_process, ev, data)
   PROCESS_PAUSE();
 
   PRINTF("RPL-Border router started\n");
-  /*
+
   // Request prefix until it has been received
   while(!prefix_set) {
     etimer_set(&et, CLOCK_SECOND);
@@ -89,9 +89,8 @@ PROCESS_THREAD(border_router_process, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     PRINTF("Waiting for prefix\n");
   }
-  */
   prefix_set = 1;
-  uip_ip6addr(&prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+//  uip_ip6addr(&prefix, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
   PRINTF("Obtained prefix: ");
   uip_debug_ipaddr_print(&prefix);
   PRINTF("\n");
